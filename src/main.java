659dadd490a2757b8c47a86737c57f1f83da7846
vphimpha <vphimpha@montgomerycollege.edu>
2019-08-09 10:51:09 -0400

    escape sequence 2

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args){
+//        Cost      Quantity   Total
+//        $1,000.00   4       $4,000.00
+//        $   50.00   8         $400.00
+//        ______________________________
+//        TOTAL:      12      $4,400.00
+
+        System.out.println("Cost\t\tQuantity\tTotal");
+        System.out.println("$1,000.00\t4\t\t\t$4,000.00");
+        System.out.println("$\t50.00\t8\t\t\t$  400.00");
+        System.out.println("__________________________________");
+        System.out.println("TOTAL:\t\t12\t\t\t$4,400.00");
+    }
+}
